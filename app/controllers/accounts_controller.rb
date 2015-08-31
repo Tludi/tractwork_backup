bb9c8aeb5f6e4b7a102257bf4291743cb1c4117f
Tludi <tludi00@gmail.com>
2015-08-31 14:19:07 -0700

    testing creating account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,31 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: []
+  
+  def index
+    @accounts = Account.all
+  end
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+    respond_to do |format|
+      if @account.save
+        format.html{ redirect_to @account, notice: 'Account creation.' }
+      else
+        redirect_to accounts_path
+      end
+    end
+  end
+
+  private
+    def set_account
+      @account = Account.find(params[:id])
+    end
+
+    def account_params
+      params[:account].permit(:name)
+    end
 end
--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:accounts)
+  end
+
+  test "should create account" do
+    assert_difference('Account.count') do
+      post :create, account: {name: 'Sample Account'}
+    end
+
+    assert_redirected_to account_path(assigns(:account))
+  end
 end
